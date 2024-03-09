47b919ae215673a950483fc1106bc364ba3da1ad
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-09 19:42:48 +0900

    Title: 변수 출력하기 3, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240309/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 2;
+        String C = "C";
+
+        System.out.println(A + "->" + B + "->" + C);
+    }
+}

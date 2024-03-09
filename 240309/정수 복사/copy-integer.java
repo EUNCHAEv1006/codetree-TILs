4719c5c69e79b391b984e60ed15384129eed7290
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-09 21:33:30 +0900

    Title: 정수 복사, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240309/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int temp;
+
+        temp = b;
+        b = a;
+
+        System.out.println(a + " " + b);
+        System.out.println(a*b);
+    }
+}

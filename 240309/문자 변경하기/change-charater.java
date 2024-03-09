2ca9909ca8162d9f19875430bf28e645c0d945c4
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-09 21:30:45 +0900

    Title: 문자 변경하기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240309/문자 변경하기/change-charater.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "a";
+        String temp = "";
+        temp = a;
+        a = "T";
+
+        System.out.println(a);
+    }
+}

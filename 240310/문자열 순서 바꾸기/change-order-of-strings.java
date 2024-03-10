8c504e617f86bf7a68d52890a96dab5d41d943f3
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-10 22:24:22 +0900

    Title: 문자열 순서 바꾸기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.next();
+        String t = scanner.next();
+
+        System.out.println(t);
+        System.out.println(s);
+    }
+}

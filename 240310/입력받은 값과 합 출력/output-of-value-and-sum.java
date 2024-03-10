7f090e9b826552223bd4cee4e68f5b16916d7a7f
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-10 19:50:26 +0900

    Title: 입력받은 값과 합 출력, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int sum = a + b;
+
+        System.out.print(a + " " + b + " " + sum);
+    }
+}

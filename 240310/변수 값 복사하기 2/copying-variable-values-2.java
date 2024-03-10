f223a2b96780615fc419dd6af4994d4807161891
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-10 19:38:54 +0900

    Title: 변수 값 복사하기 2, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240310/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        a = b = c;
+
+        System.out.print(a + " " + b + " " + c);
+    }
+}

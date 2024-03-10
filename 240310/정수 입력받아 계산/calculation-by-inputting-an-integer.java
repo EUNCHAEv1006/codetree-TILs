7a288f92dc44d1d5c39ebbbc7f69aa8125ee8b35
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-10 19:42:13 +0900

    Title: 정수 입력받아 계산, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int answer = (a * 2) + 3;
+
+        System.out.println(answer);
+    }
+}

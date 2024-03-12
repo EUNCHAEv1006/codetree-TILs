754ac2c64bad579f2565d72500dc0acd0ce64f81
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-12 23:35:50 +0900

    Title: 두 사람, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/두 사람/two-person.java
+++ 240312/두 사람/two-person.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int ageA = sc.nextInt();
-        String sexA = sc.next();
 
-        int ageB = sc.nextInt();
-        String sexB = sc.next();
+int a_year = sc.nextInt();
+String a_sex = sc.next().toString(); 
 
-        if((ageA >= 19 && ageB >= 19) || (sexA.equals('M') && sexB.equals('M'))) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+int b_year  = sc.nextInt();
+String b_sex = sc.next().toString(); 
+
+if( (a_year >=19 && a_sex.equals("M")) || (b_year >=19 && b_sex.equals("M"))) {
+    System.out.println(1);
+}else {
+    System.out.println(0);
+}
     }
-
 }

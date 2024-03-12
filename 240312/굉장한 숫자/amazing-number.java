04c7c703185c523e123e2be0176ca24994097cf4
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-12 17:25:09 +0900

    Title: 굉장한 숫자, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240312/굉장한 숫자/amazing-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if(n%2 !=0 || n%3==0) {
+            System.out.println("true");
+        } else if (n%2==0 || n%5==0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}

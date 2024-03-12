f2191b3588f6b637380eddbb463e928875aca746
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-13 00:42:52 +0900

    Title: 윤년인가, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/윤년인가/is-leap-year.java
+++ 240313/윤년인가/is-leap-year.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int y = scanner.nextInt();
+        // 변수 선언
+        int y;
+        
+        // 입력
+        y = sc.nextInt();
 
-        if(y%4 == 0) {
-            System.out.println("true");
-        } else if((y%100 ==0) && (y%400 != 0)) {
-            System.out.println("false");
-        } else {
+        // 출력
+        if(y % 4 == 0) {
+            if(y % 100 == 0) {
+                if(y % 400 == 0) {
+                    System.out.println("true");
+                }
+                else {
+                    System.out.println("false");
+                }
+            }
+            else {
+                System.out.println("true");
+            }
+        }
+        else {
             System.out.println("false");
         }
     }

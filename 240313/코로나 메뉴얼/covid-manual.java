e2d93809df2e9f20e1eb2c655eee8c5cccb2acee
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-13 01:09:03 +0900

    Title: 코로나 메뉴얼, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240313/코로나 메뉴얼/covid-manual.java
+++ 240313/코로나 메뉴얼/covid-manual.java
@@ -2,42 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        String coldA = scanner.next().toString();
-        int tempA = scanner.nextInt();
+        // 변수 선언
+        char c1, c2, c3;
+        int t1, t2, t3;
+        
+        // 입력
+        c1 = sc.next().charAt(0);
+        t1 = sc.nextInt();
+        c2 = sc.next().charAt(0);
+        t2 = sc.nextInt();
+        c3 = sc.next().charAt(0);
+        t3 = sc.nextInt();
 
-        String coldB = scanner.next().toString();
-        int tempB = scanner.nextInt();
-
-        String coldC = scanner.next().toString();
-        int tempC = scanner.nextInt();
-
-        String result;
-
-        if(coldA.equals("Y") && (tempA >= 37)) {
-            if((coldB.equals("Y") && (tempB >= 37)) || (coldC.equals("Y")) && (tempC >= 37)) {
+        // 출력
+        // A가 2명 이상인지 판단하기
+        if(c1 == 'Y' && t1 >= 37) {
+            // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+            if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
                 System.out.println("E");
-            } else {
+            else
                 System.out.println("N");
-            }
         }
-
-        if(coldA.equals("N") && (tempA >= 37)) {
-            if((coldB.equals("N") && (tempB >= 37)) || coldC.equals("N") && (tempC >= 37)) {
+        else {
+            // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+            if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
                 System.out.println("N");
-            }else {
-                System.out.println("N");
-            }
         }
-
-        if(coldA.equals("Y") && (tempA < 37)) {
-            if((coldB.equals("Y") && (tempB < 37)) || coldC.equals("Y") && (tempC < 37)) {
-                System.out.println("N");
-            }else {
-                System.out.println("N");
-            }
-        }
-
     }
 }

f2166fbbb2bc6eb852d529d2963d2f4d3a3d6779
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-14 16:13:29 +0900

    Title: 별 n개 출력하기, Time: 3081ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

---
+++ 240314/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=1; 1<=50; i++) {
+            System.out.println("*");
+        }
+    }
+}

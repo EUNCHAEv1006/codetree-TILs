265b3cc41f55209881d1da2b9c3a88e385c5f9a3
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-03-15 09:43:00 +0900

    Title: 자연수면 출력, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240315/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a>0) {
+            for(int i=1; i<=b; i++) {
+                System.out.print(a);
+            }
+        }
+    }
+}

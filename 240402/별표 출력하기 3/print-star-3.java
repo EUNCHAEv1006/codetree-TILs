7e7a55df8d32b0c0ec5f36f9b0884203883a8e38
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-04-02 16:44:59 +0900

    Title: 별표 출력하기 3, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // 모양에 맞게 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for(int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

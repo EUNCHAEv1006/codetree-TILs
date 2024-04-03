d416b69d7ae051cb9fb587a8bb7b26aaad7416c3
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-04-03 23:34:01 +0900

    Title: 별표 출력하기 9, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240403/별표 출력하기 9/print-star-9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j>1; j--){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

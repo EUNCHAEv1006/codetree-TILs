8e935709309650f445e071573d50644f341ffa12
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-04-03 23:34:20 +0900

    Title: 별표 출력하기 9, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240403/별표 출력하기 9/print-star-9.java
+++ 240403/별표 출력하기 9/print-star-9.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n = scanner.nextInt();
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
 
-        for(int i=0; i<n; i++){
-            for(int j=n-i; j>1; j--){
-                System.out.print(" ");
-            }
-            for(int j=0; j<2*i+1; j++){
+        // 모양에 맞게 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = n - i; j > 1; j--)
+                System.out.print("  ");
+            for(int j = 0; j < 2 * i + 1; j++)
                 System.out.print("* ");
-            }
             System.out.println();
         }
     }

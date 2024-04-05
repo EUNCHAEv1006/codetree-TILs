26a56708e6f6043573ed42fc8c5509f639374236
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-04-06 01:51:59 +0900

    Title: 체크 다이아몬드, Time: 229ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n; i++){
+
+            for(int j=n; j>i+1; j--){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=1; i<n; i++){
+
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=n; j>i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

b35d3384838f59d6473d6eec2aca971c69825064
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-04-06 01:42:06 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240406/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 240406/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -2,27 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int n = scanner.nextInt();
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
 
-        for(int i=0; i<n; i++){
-
-            for(int j=n - 1 - i; j>=0; j--){
-                System.out.print(" ");
-            }
-
-            for(int j=0; j<i+1; j++){
+        // 모양에 맞게 윗쪽 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++)
+                System.out.print("  ");
+            for(int j = 0; j <= i; j++)
                 System.out.print("@ ");
-            }
             System.out.println();
         }
-
-        for(int i=0; i<n-1; i++){
-
-            for(int j=0; j<n - 1 - i; j++){
+        
+        // 모양에 맞게 아랫쪽 별을 출력합니다.
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++)
                 System.out.print("@ ");
-            }
             System.out.println();
         }
     }

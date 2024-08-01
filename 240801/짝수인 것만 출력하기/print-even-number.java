4b8560ae25a37a839626138e2ce6a2c5e9c84af7
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-08-01 11:15:03 +0900

    Title: 짝수인 것만 출력하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/짝수인 것만 출력하기/print-even-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+
+            if(arr[i]%2==0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
